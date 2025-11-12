5f366ef8b85f0a951b0ac96d8d9543e06a1eb82d
namigismylvv <11869@holbertonstudents.com>
2025-11-12 04:10:23 +0400

    feat

--- pld_task11/index.js
+++ pld_task11/index.js
@@ -1,14 +1,21 @@
-const aside =  document.querySelector("#offcanvas")
-const burgerBtn = document.querySelector(".button--burger")
-const closeBtn = document.querySelector(".offcanvas--close")
+const aside =  document.getElementById("offcanvas")
+const burgerBtn = document.getElementById("burgerBtn")
+const closeBtn = document.getElementById("closeBtn")
+const overlay = document.getElementById("overlay")
 burgerBtn.addEventListener("click",()=>{
     aside.classList.toggle("active")
+    overlay.classList.toggle("active")
     
 })
-
 closeBtn.addEventListener("click",()=>{
     aside.classList.remove("active")
-    
+    overlay.classList.remove("active")
 })
+overlay.addEventListener("click",()=>{
+aside.classList.remove("active")
+overlay.classList.remove("active")
+})
+
+
 
 
